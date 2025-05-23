9fea4a7054221e3769540dd21ae25b5fdbee0b78
MatZar01 <zski.mateusz@gmail.com>
2025-05-23 15:45:24 +0200

    print style update

--- src/print_style.py
+++ src/print_style.py
@@ -4,3 +4,19 @@ class Style():
     BLUE = "\033[34m"
     ORANGE = "\33[93m"
     RESET = "\033[0m"
+
+    @classmethod
+    def red(cls, text):
+        return f'{cls.RED}{text}{cls.RESET}'
+
+    @classmethod
+    def green(cls, text):
+        return f'{cls.GREEN}{text}{cls.RESET}'
+
+    @classmethod
+    def blue(cls, text):
+        return f'{cls.BLUE}{text}{cls.RESET}'
+
+    @classmethod
+    def orange(cls, text):
+        return f'{cls.ORANGE}{text}{cls.RESET}'

95600f34422ca2576a231c481b33d1c60a9afd3e
MatZar01 <zski.mateusz@gmail.com>
2025-08-04 19:56:45 +0200

    resutls_add file upd

--- results_add.py
+++ results_add.py
@@ -3,6 +3,7 @@ import yaml
 import numpy as np
 import os
 from src import Style
+import matplotlib.pyplot as plt
 
 m_path = '/Users/mateusz/Desktop/Multi_Forecasting/log_dir/rand'
 experiment_list = [f.path for f in os.scandir(m_path) if f.is_dir()]
@@ -25,3 +26,9 @@ for i in logs[0]:
     head_counts.append([logs[0][i]['head_count'], logs[1][i]['head_count'], logs[2][i]['head_count']])
     head_selects.append([logs[0][i]['selected_head'], logs[1][i]['selected_head'], logs[2][i]['selected_head']])
     test_res.append([logs[0][i]['test'], logs[1][i]['test'], logs[2][i]['test']])
+
+#head_counts = np.array(head_counts)
+# counts - min / avg / max
+#head_c = np.concatenate([[np.min(head_counts, axis=1)], [np.mean(head_counts, axis=1)], [np.max(head_counts, axis=1)]])
+#%%
+# tasks per head
